c0704c6240a4b0bb356edf9f00defe9bc836601e
mafese3 <marinaserranof@gmail.com>
2025-11-20 22:12:54 +0100

    Modified Serie class.

--- src/Serie.java
+++ src/Serie.java
@@ -17,30 +17,64 @@ public class Serie extends Contenido {
 
         sumaCalificaciones = 0;
         calificacionMedia = 0;
-
-        usuario.addCreacion(this);
     }
 
-
-
+    /**
+     * @return El número de temporadas.
+     */
 	public int getNumeroTemporadas() {
 		return this.numeroTemporadas;
 	}
 
 	/**
-	 * 
-	 * @param numeroTemporadas
+	 * @param numeroTemporadas El número de temporadas.
 	 */
 	public void setNumeroTemporadas(int numeroTemporadas) {
 		this.numeroTemporadas = numeroTemporadas;
 	}
 
+    /**
+     * @return La calificación media de la serie.
+     */
+    public double getCalificacionMedia() {
+        return this.calificacionMedia;
+    }
 
-    /// Se añade un recurso a la lista
+    /**
+     * Añade un recurso en el que ha sido referenciada la serie.
+     * @param recurso El recurso a añadir.
+     */
     public void addRecurso(RecursoSerie recurso) {
         this.recursos.add(recurso);
+
+        //Actualizamos la calificación media del contenido.
         sumaCalificaciones += recurso.getCalificacion();
         calificacionMedia = sumaCalificaciones / recursos.size();
+
+        System.out.println("Recurso añadido. Calificación media de la serie: " + this.calificacionMedia);
+    }
+
+    /**
+     * Elimina un recurso en el que estaba referenciada la serie.
+     * @param recurso El recurso a eliminar.
+     */
+    public void removeRecurso(RecursoSerie recurso) {
+        this.recursos.remove(recurso);
+
+        //Actualizamos la calificación media del contenido.
+        sumaCalificaciones -= recurso.getCalificacion();
+        calificacionMedia = sumaCalificaciones / recursos.size();
+
+        System.out.println("Recurso eliminado. Calificación media del álbum: " + this.calificacionMedia);
+    }
+
+    /**
+     * Devuelve una cadena con las propiedades necesarias para que defina a la serie.
+     * @return String descriptiva de la serie.
+     */
+    public String toString() {
+        return ("Título: " + this.getNombre() + ", Calificación media: " + this.getCalificacionMedia() +
+                ", Creada por: " + this.getCreador());
     }
 
 }
